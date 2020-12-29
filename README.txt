dbfb971df641df2a1219f1f591b82d06c4b6956a
L4v <ivos.jovan@protonmail.ch>
2020-12-29 23:39:37 +0100

    :rocket: Initial commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+![](ficus.svg =160px)
+# Ficus
+A 4AM coffee fueled, team building, plant inspired side project  
+
+## What can it do
+The mighty Ficus can come and go as ~~it pleases~~ you will it and  
+can also share it's infinite wisdom acquired throught it's lifetime.  
+For a full list of features type the following command in chat:  
+```
+!ficus help
+```
+
---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import discord
+from discord.ext import commands
+import random
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+QUOTES_FILENAME = "quotes.txt"
+HELP_FILENAME = "help.txt"
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name="ficus")
+async def ficus_says(ctx, arg1="", arg2=""):
+
+    ficus_quotes = []
+    with open(QUOTES_FILENAME) as f:
+        ficus_quotes = [line.rstrip() for line in f]
+
+    if arg1 == "come":
+        await ficus_come(ctx)
+    elif arg1 == "shoo":
+        await ficus_shoo(ctx)
+    elif arg1 == "kokain":
+        response = "Gde mi je kasicica..."
+        await ctx.send(response)
+    elif arg1 == "help":
+        response = ""
+        with open(HELP_FILENAME) as f:
+            response = f.read()
+        await ctx.send(response)
+    elif arg1 == "newest":
+        response = ficus_quotes[-1]
+        await ctx.send(response)
+    elif arg1 == "join" and arg2 is not None:
+        await ficus_join(ctx, arg2)
+    else:
+        response = random.choice(ficus_quotes)
+        await ctx.send(response)
+
+#@bot.command(pass_context=True, name="ficus_here_boy")
+async def ficus_come(ctx):
+    await ficus_disconnect(ctx)
+    user = ctx.author
+    channel = user.voice.channel
+    await channel.connect()
+    await ficus_ciao(ctx)
+    await ctx.send("Eo mene opet, eo mene opet ja")
+
+async def ficus_join(ctx, channel_name):
+    channel = next((c for c in ctx.guild.voice_channels if c.name == channel_name), None)
+    if channel is not None:
+        await ficus_disconnect(ctx)
+        await channel.connect()
+        await ficus_ciao(ctx)
+
+#@bot.command(name="ficus_shoo")
+async def ficus_shoo(ctx):
+    await ficus_disconnect(ctx)
+    await ctx.send("Bye " + ctx.author.name)
+
+async def ficus_disconnect(ctx):
+    if ctx.voice_client is not None:
+        await ctx.voice_client.disconnect()
+
+async def ficus_ciao(ctx):
+    guild = ctx.guild
+    voice_client = ctx.voice_client
+    if voice_client is not None:
+        audio_source = discord.FFmpegPCMAudio("cao.mp3")
+    if not voice_client.is_playing():
+        voice_client.play(audio_source, after=None)
+
+bot.run(TOKEN)
